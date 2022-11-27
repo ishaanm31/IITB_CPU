096ff2658c085564a986a887410a0582ec44a7c9
IaintaWeeb <118739396+IaintaWeeb@users.noreply.github.com>
2022-11-27 21:03:16 +0530

    IITB_CPU
    Complete Project Directory

--- IITB_CPU.vhd
+++ IITB_CPU.vhd
@@ -5,13 +5,15 @@ use ieee.numeric_std.all;
 entity IITB_CPU is
     port (
         --Clock and reset
-        clock,Reset_ext: in std_logic;
+        clock,Reset: in std_logic;
         --Input for External Memory Update
         Mem_Ext_WR:in std_logic;
         Mem_Ext_Data_in,Mem_Ext_Add : in std_logic_vector(15 downto 0);
         --Outputing Program Counter(T1) And T3 for Testing purposes
         PC_test,T3_test: out std_logic_vector(15 downto 0)
-    )
+    );
+end entity;
+
 architecture arch of IITB_CPU is
 --Initiating FSM and Datapaths
     component FSM is
@@ -19,7 +21,7 @@ architecture arch of IITB_CPU is
             --OutPuts from the Datapath
             Z_flag, C_flag :in std_logic;
             T2_out :in std_logic_vector(15 downto 0);
-            loop_count:in std_logic_vector(2 downto 0);
+            loop_count:in std_logic_vector(15 downto 0);
 
             --Inputs to the Datapath
             alu_sel:out std_logic_vector(1 downto 0);    
@@ -55,10 +57,10 @@ architecture arch of IITB_CPU is
             --Inputs for the FSM
             Z_flag, C_flag: out std_logic;
             T1_out,T2_out ,T3_out: buffer std_logic_vector(15 downto 0);
-            loop_count:buffer std_logic_vector(2 downto 0);
+            loop_count:buffer std_logic_vector(15 downto 0);
             --External Memory Control
             Mem_Ext_WR:in std_logic;
-            Mem_Ext_Data_in,Mem_Ext_Add : in std_logic_vector(15 downto 0);
+            Mem_Ext_Data_in,Mem_Ext_Add : in std_logic_vector(15 downto 0)
             );
     end component;
 --Initiating Signals to connect FSM to Datapath and mapping Input outputs of the whole machine
@@ -80,10 +82,10 @@ architecture arch of IITB_CPU is
             signal S_Z_flag, S_C_flag:  std_logic;
         --Register T2(Instructions) and Loop Counter for LM and SM
             signal  S_T2_out : std_logic_vector(15 downto 0);
-            signal S_loop_count: std_logic_vector(2 downto 0);
+            signal S_loop_count: std_logic_vector(15 downto 0);
     --Test Vector Output of the Machine
         signal S_T1_out,S_T3_out:  std_logic_vector(15 downto 0);
-        signal S_loop_count: std_logic_vector(2 downto 0);
+        
 begin
     My_FSM: FSM port map(
         clock=>clock, reset=>reset, 
@@ -127,13 +129,13 @@ begin
         loop_count_WR=> S_loop_count_WR,
         ALU_A_sel=> S_ALU_A_sel,
         ALU_B_sel=> S_ALU_B_sel,
-        T3_sel=>S_T3_sel , Mem_Add_Sel=> S_Mem_Add_Sel, Mem_In_Sel=>S_Mem_In_Sel
+        T3_sel=>S_T3_sel , Mem_Add_Sel=> S_Mem_Add_Sel, Mem_In_Sel=>S_Mem_In_Sel,
         --Outputs for testing
         T1_out=>PC_test,
         T3_out=>T3_test,
         --Input pins for memory control
         Mem_Ext_WR=>Mem_Ext_WR,
         Mem_Ext_Data_in=> Mem_Ext_Data_in,
-        Mem_Ext_Add =>Mem_Ext_Add,
+        Mem_Ext_Add =>Mem_Ext_Add
     );
 end architecture arch;
